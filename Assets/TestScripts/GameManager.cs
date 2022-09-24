531bc5bf55f4d7cf6e64c208d6984aa74c30ce41
James Oakes <cheerlesssnake@gmail.com>
2022-09-24 12:38:45 -0500

    Fixed player duplication bug

--- Assets/TestScripts/GameManager.cs
+++ Assets/TestScripts/GameManager.cs
@@ -62,15 +62,19 @@ namespace net.EthanTFH.BTSGameJam
                 if(PlayerController.LocalPlayerInstance == null)
                 {
                     Debug.LogFormat("Making a new LocalPlayer for {0}", SceneManagerHelper.ActiveSceneName);
-                    
+
                     if (GameObject.Find("Floor"))
-                        PhotonNetwork.Instantiate(this.playerPrefabBig.name, new Vector3(0f, GameObject.Find("Floor").transform.position.y + 5, 0f), Quaternion.identity, 0);
+                    {
+                        PhotonNetwork.Instantiate(this.playerPrefabBig.name, new Vector3(0f, GameObject.Find("Floor").transform.position.y + 8, 0f), Quaternion.identity, 0);
+                    }
+                    else
+                        PhotonNetwork.Instantiate(this.playerPrefabBig.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+
                     if (PhotonNetwork.CurrentRoom.PlayerCount == 2) 
                     {
                         Debug.Log("Player Count 2");
                     }
-                    else
-                        PhotonNetwork.Instantiate(this.playerPrefabBig.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+                   
 
                 }
                 else
